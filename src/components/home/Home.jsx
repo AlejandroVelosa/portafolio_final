a713e798f2c0c57525c2e518f488624648e3c565
Alejandro velosa <alejovelosa4@gmail.com>
2023-08-29 09:53:06 -0500

    se cambia imagen de perfil

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Profile from '../../assets/newMe.png';
+import Profile from '../../assets/imagenportfolio.png';
 import { Link } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
 import './Home.css';
